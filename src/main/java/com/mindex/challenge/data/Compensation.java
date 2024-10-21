34ae26403c3ecf8af7ff52cced0e990bd8db3c7b
jmrossi98 <jmrossi98@gmail.com>
2024-10-21 16:16:23 -0400

    Added compensation data model class

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,36 @@
+package com.mindex.challenge.data;
+
+import java.util.List;
+
+public class Compensation {
+    private String employeeId;
+    private double salary;
+    private LocalDate effectiveDate;
+
+    public Compensation() {
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
